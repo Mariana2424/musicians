0163860cd5920da6a8afe5a7b1fd8a9e060537a9
Mariana <marianagogashvili24@gmail.com>
2017-08-30 12:35:03 +0300

    Last

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,59 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update]
+  def new
+    @article = Article.new
+  end
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def destroy
+    
+    @article.destroy
+    redirect_to articles_path
+  end
+  private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :image)
+    end
+
+    def require_same_user
+      if current_user != @article.user
+        redirect_to root_path
+      end
+    end
+end
